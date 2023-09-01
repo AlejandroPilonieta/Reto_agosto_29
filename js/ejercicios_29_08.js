c67a956989d17de61f52bf4e3edc9c40486260bc
AlejandroPilonieta <alejandropilonieta@gmail.com>
2023-08-31 19:47:28 -0500

    Reto_agosto_29

--- js/ejercicios_29_08.js
+++ js/ejercicios_29_08.js
@@ -4,7 +4,8 @@ const btnExerciseThree = document.getElementById("btnExercise3");
 const btnExerciseFour = document.getElementById("btnExercise4");
 const btnExerciseFive = document.getElementById("btnExercise5");
 const btnExerciseSix = document.getElementById("btnExercise6");
-
+const btnExerciseSeven = document.getElementById("btnExercise7");
+const btnExerciseEight = document.getElementById("btnExercise8");
 
 /**
  * Ejercicio ciclo for nomal
@@ -85,7 +86,7 @@ function ejercicioTres() {
 /**
  * crear programa que encuentre el numero más grande de un array usando for y lo imprima en alert o .log matriz [45, 23, 67, 89, 12, 56]
  *
- * SE DEBE CAMBIAR EL NUMERO EN EL IF
+ * 
  */
 function ejercicioCuatro() {
   const numeros = ["45", "23", "67", "89", "12", "56"];
@@ -116,13 +117,55 @@ function ejercicioCinco() {
   return alert(`La suma de los elementos del array es: ${suma}`);
 }
 
+//: Crea un programa que multiplique cada elemento de un array por 2 y devuelva un nuevo array con los resultados usando for normal. Matriz de ejemplo: [3, 7, 2, 8]
 
 function ejercicioSeis() {
-  alert("entro 6")
+  const numeros = ["3", "7", "2", "8"];
+  const multiplicador = 2;
+  let resultado = [];
 
-
+  for (let i = 0; i < numeros.length; i++) {
+    const numero = parseFloat(numeros[i]); // convierto en numeros el array
+    const nuevoArray = numero * multiplicador;
+    resultado.push(nuevoArray);
+  }
+  console.log(resultado); // Mostrar el nuevo array con los resultados en consola
+  return resultado;
 }
 
+// Crea un programa que encuentre la suma de los elementos pares (los impares no los suma) en un array usando forEach. Array de ejemplo: [1, 4, 7, 3, 10, 12] muestre el resultado en consola
+
+function ejercicioSiete() {
+  const numeros = [1, 4, 7, 3, 10, 12];
+  let resultado = 0;
+
+  numeros.forEach((numero) => {
+    if (numero % 2 === 0) {
+      resultado += numero;
+    }
+  });
+  console.log(resultado);
+  return resultado;
+}
+
+// Crea un programa que encuentre el primer número mayor que 50 en un array de números usando un bucle while. Matriz de ejemplo: [30, 45, 60, 72, 48, 10]
+
+function ejercicioOcho() {
+  const numeros = [30, 45, 60, 72, 48, 10];
+  let resultado = 0;
+
+  while (resultado < numeros.length) {
+    if (numeros[resultado] > 50) {
+      resultado = numeros[resultado];
+      break;
+    }
+    resultado++;
+  }
+  console.log(resultado);
+  return resultado;
+}
+
+//onclick
 
 btnExerciseOne.onclick = function () {
   ejercicioUno();
@@ -141,4 +184,10 @@ btnExerciseFive.onclick = function () {
 };
 btnExerciseSix.onclick = function () {
   ejercicioSeis();
+};
+btnExerciseSeven.onclick = function () {
+  ejercicioSiete();
+};
+btnExerciseEight.onclick = function () {
+  ejercicioOcho();
 };
